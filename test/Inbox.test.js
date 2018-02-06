eb36e0dd2ccd0590c96cc2282b62f4765610dcc6
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-05 19:48:33 -0700

    Log output of assembled/deployed contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 let accounts;
 
@@ -10,11 +11,14 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!']})
+        .send({ from: accounts[0], gas: '1000000' })
 
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     });
 });
